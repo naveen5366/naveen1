54320e27075e06873d5ea22e53ca5c991d7e550a
naveen5366 <naveennaveen44613@gmail.com>
2024-09-27 10:52:42 +0530

    Add files via upload

---
+++ simple.java
@@ -0,0 +1,9 @@
+class Test
+{
+    public static void main(String []args)
+    {
+        System.out.println("My First Java Program.");
+        System.out.println("this is devops lab.");
+        System.out.println("we are the students of cse-A1.");
+    }
+};
